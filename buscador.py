a5275221c592f925c512e19d85466d404fae3186
SergioGeek <sergio.it.geek@gmail.com>
2018-05-05 19:25:08 +0200

    añadido la primera mejora pero obtengo peores valores

--- buscador.py
+++ buscador.py
@@ -18,10 +18,10 @@ if __name__ == "__main__":
 
 
 
-	consulta = "La Asociación Española Contra el Cáncer con sede en Linares, organiza la 3ª edición de NOCHE MÁGICA DORADA,que será el próximo 22 de Octubre a las 8 de la tarde en el Teatro Cervanters de Linares,dónde actuarán grandes artistas, como la gran pianista Marisa Montiel, las sopranos Mº Eugenia Boix,Cecilia Gallego, Susana Jannes y los tenores Joaquín Robles y Francisco Heredia, como pianista acompañante Mariano Hernández. Posteriormente se realizará una cena-cóctel en el Hotel Anibal en dónde habrá un espectáculo flamenco a cargo del Ballet de Mª del Mar Ramírez y Raquel Parrilla."
+	querySTR = "La Asociación Española Contra el Cáncer con sede en Linares, organiza la 3ª edición de NOCHE MÁGICA DORADA,que será el próximo 22 de Octubre a las 8 de la tarde en el Teatro Cervanters de Linares,dónde actuarán grandes artistas, como la gran pianista Marisa Montiel, las sopranos Mº Eugenia Boix"
 
 
-	tokenizer = Tokenizer( consulta )
+	tokenizer = Tokenizer( querySTR )
 
 	tokenizer.tokenize()
 
@@ -35,7 +35,7 @@ if __name__ == "__main__":
 
 	query = Query()
 
-	query.add( consulta, stemmer.stmDic )
+	query.add( querySTR, stemmer.stmDic )
 
 	#print( query.index )
 
@@ -98,5 +98,100 @@ if __name__ == "__main__":
 
 	result.reverse()
 
+	#print( result )
+
+	###Pseudorealimentación por relevancia (PRF)
+
+	newWords = []	
+	
+	for i in range( 5 ):
+
+		aux = index.index[ index.nameIndex.index( result[i][0] ) ]
+
+		aux = sorted( aux.items(), key = operator.itemgetter(1) )
+		
+		aux.reverse()
+
+		for j in range( 5 ):
+
+			querySTR += " " + aux[j][0]
+
+
+
+	tokenizer = Tokenizer( querySTR )
+
+	tokenizer.tokenize()
+
+	stopper = Stopper()
+
+	stopper.stopping( tokenizer.tokens  )
+
+	stemmer = Stemmer()
+
+	stemmer.stemming( stopper.stoppedList )
+
+	query = Query()
+
+	query.add( querySTR, stemmer.stmDic )
+
+	#print( query.index )
+
+	query.normalize()
+
+	#print( query.normalizeIndex )
+
+	query.IDFs = index.IDFs
+
+	#print( indexQ.IDFs)
+
+	query.weightsCalc()
+
+	#print( query.weights )
+
+	#print( max(query.weights.values()) )
+
+	query.normCalc()
+
+	#print( query.norm )
+
+	query.normalizeWeightsCalc()
+
+	#print( query.normalizeWeights )
+
+	query.normalizeNormCalc()
+	index.normalizeNormCalc()
+
+	###Cálculo de la Similitud
+	sim = {}
+
+	
+	for inw, val in enumerate(index.normalizeWeights):
+
+		summ = 0
+
+		for iqnw in query.normalizeWeights:
+
+			if iqnw in val:
+
+				summ += query.normalizeWeights[iqnw] * val[iqnw]
+
+		calc = summ / (index.norm[inw] * query.normalizeNorm)
+
+		if calc != 0:
+
+			sim[index.nameIndex[inw]] = calc
+
+
+	
+	
+
+	result = sorted( sim.items(), key = operator.itemgetter(1) )
+
+	result.reverse()
+
 	print( result )
+
+
+
+
 
